d6802f3b95c7eeed40f4514836aa20c656eb9000
Samuel Domínguez <samueldominguez@protonmail.com>
2025-09-07 19:04:09 +0100

    hey

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -20,8 +21,27 @@ type TwilioMedia struct {
 	Event     string `json:"event"`
 	StreamSid string `json:"streamSid"`
 	Media     struct {
+		Track   string `json:"track"`
 		Payload string `json:"payload"`
 	} `json:"media"`
+}
+
+func createWavHeader(dataLength int) []byte {
+	header := make([]byte, 44)
+	copy(header[0:4], []byte("RIFF"))
+	binary.LittleEndian.PutUint32(header[4:8], uint32(36+dataLength)) // File size - 8
+	copy(header[8:12], []byte("WAVE"))
+	copy(header[12:16], []byte("fmt "))
+	binary.LittleEndian.PutUint32(header[16:20], 16)   // fmt chunk size
+	binary.LittleEndian.PutUint16(header[20:22], 7)    // μ-law format
+	binary.LittleEndian.PutUint16(header[22:24], 1)    // Mono
+	binary.LittleEndian.PutUint32(header[24:28], 8000) // Sample rate
+	binary.LittleEndian.PutUint32(header[28:32], 8000) // Byte rate
+	binary.LittleEndian.PutUint16(header[32:34], 1)    // Block align
+	binary.LittleEndian.PutUint16(header[34:36], 8)    // Bits per sample
+	copy(header[36:40], []byte("data"))
+	binary.LittleEndian.PutUint32(header[40:44], uint32(dataLength)) // Data size
+	return header
 }
 
 func main() {
@@ -40,6 +60,9 @@ func main() {
 
 	// WebSocket handler
 	http.HandleFunc("/stream", func(w http.ResponseWriter, r *http.Request) {
+		var audioBuffer []byte // Accumulate μ-law data for this call
+		var audioFile *os.File // File for this call
+		var streamSid string   // Unique identifier for the call
 		log.Println("stream started")
 		log.Println("headers:", r.Header)
 		ws, err := upgrader.Upgrade(w, r, nil)
@@ -53,25 +76,57 @@ func main() {
 			_, msg, err := ws.ReadMessage()
 			if err != nil {
 				log.Println("WebSocket read error:", err)
+				// Write WAV header and data before closing
+				if len(audioBuffer) > 0 && audioFile != nil {
+					header := createWavHeader(len(audioBuffer))
+					if _, err := audioFile.Write(header); err != nil {
+						log.Println("Error writing WAV header:", err)
+					}
+					if _, err := audioFile.Write(audioBuffer); err != nil {
+						log.Println("Error writing WAV data:", err)
+					}
+					audioFile.Close()
+					log.Printf("Saved audio to call_%s.wav", streamSid)
+				}
 				return
 			}
 
 			log.Println(string(msg))
 			// Parse incoming Twilio message
-			var media TwilioMedia
-			if err := json.Unmarshal(msg, &media); err != nil {
+			var twilioMsg TwilioMedia
+			if err := json.Unmarshal(msg, &twilioMsg); err != nil {
 				log.Println("JSON parse error:", err)
 				continue
 			}
+			if twilioMsg.Event == "start" {
+				streamSid = twilioMsg.StreamSid
+				audioFile, err = os.Create("call_" + streamSid + ".wav")
+				if err != nil {
+					log.Println("Error creating WAV file:", err)
+					return
+				}
+				audioBuffer = nil // Reset buffer for new call
+				log.Printf("Created file for call_%s.wav", streamSid)
+			}
 
-			// Respond to media event with pre-recorded audio
-			if media.Event == "media" {
-				response := TwilioMedia{
-					Event:     "media",
-					StreamSid: media.StreamSid,
-					Media: struct {
-						Payload string `json:"payload"`
-					}{Payload: base64Audio},
+			// Save inbound audio from "media" event
+			if twilioMsg.Event == "media" && twilioMsg.Media.Track == "inbound" {
+				audioData, err := base64.StdEncoding.DecodeString(twilioMsg.Media.Payload)
+				if err != nil {
+					log.Println("Base64 decode error:", err)
+					continue
+				}
+				audioBuffer = append(audioBuffer, audioData...)
+				log.Println("Appended audio chunk, total length:", len(audioBuffer))
+			}
+
+			if twilioMsg.Event == "start" {
+				response := map[string]interface{}{
+					"event":     "media",
+					"streamSid": twilioMsg.StreamSid,
+					"media": map[string]string{
+						"payload": base64Audio,
+					},
 				}
 				responseJSON, err := json.Marshal(response)
 				if err != nil {
@@ -82,6 +137,7 @@ func main() {
 					log.Println("WebSocket write error:", err)
 					return
 				}
+				log.Println("Sent μ-law audio response")
 			}
 		}
 	})
